6806cf7fdeab11589b7b346f697ec323d53af624
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-04-16 13:23:47 +0800

    :wrench: conf(extension/vite): 配置vite打包

---
+++ extension/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    target: 'node16',
+    emptyOutDir: true,
+    lib: {
+      entry: './src/extension.ts',
+      formats: ['cjs'],
+      name: 'hello',
+      fileName: 'extension.js'
+    },
+    rollupOptions: {
+      external: ['vscode'],
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: 'assers/[name].[ext]'
+      }
+    }
+  }
+})
