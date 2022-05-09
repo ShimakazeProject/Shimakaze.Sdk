78d50513e90944bc2c19ddf57f99f7d557c8934f
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-05-09 16:18:01 +0800

    :sparkles: feat(.net/utils): 添加了一些扩展方法

---
+++ src/Shimakaze.Sdk.Utils/AsyncDisposableExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Shimakaze.Sdk.Utils;
+
+public static class AsyncDisposableExtensions
+{
+    public static async Task AwaitUsing<T>(this T resource, Action<T> action) where T : IAsyncDisposable
+    {
+        await using T res = resource;
+        action(res);
+    }
+    public static async Task<TResult> AwaitUsing<T, TResult>(this T resource, Func<T, TResult> action) where T : IAsyncDisposable
+    {
+        await using T res = resource;
+        return action(res);
+    }
+
+    public static async Task AwaitUsing<T>(this T resource, Func<T, Task> action) where T : IAsyncDisposable
+    {
+        await using T res = resource;
+        await action(res);
+    }
+    public static async Task<TResult> AwaitUsing<T, TResult>(this T resource, Func<T, Task<TResult>> action) where T : IAsyncDisposable
+    {
+        await using T res = resource;
+        return await action(res);
+    }
+}
---
+++ src/Shimakaze.Sdk.Utils/UsingExtension.cs
@@ -0,0 +1,37 @@
+﻿namespace Shimakaze.Sdk.Utils;
+
+public static class DisposableExtensions
+{
+    public static void Using<T>(this T resource, Action<T> action) where T : IDisposable
+    {
+        using T res = resource;
+        action(res);
+    }
+    public static TResult Using<T, TResult>(this T resource, Func<T, TResult> action) where T : IDisposable
+    {
+        using T res = resource;
+        return action(res);
+    }
+
+    public static async Task UsingAsync<T>(this T resource, Func<T, Task> action) where T : IDisposable
+    {
+        using T res = resource;
+        await action(res);
+    }
+    public static async Task<TResult> Using<T, TResult>(this T resource, Func<T, Task<TResult>> action) where T : IDisposable
+    {
+        using T res = resource;
+        return await action(res);
+    }
+
+
+}
+
+public static class SystemExtensions
+{
+    public static void Then<T>(this T @this, Action<T> action) => action(@this);
+    public static TResult Then<T, TResult>(this T @this, Func<T, TResult> action) => action(@this);
+
+    public static void Then<T1, T2>(this ValueTuple<T1, T2> @this, Action<T1, T2> action) => action(@this.Item1, @this.Item2);
+    public static TResult Then<T1, T2, TResult>(this ValueTuple<T1, T2> @this, Func<T1, T2, TResult> action) => action(@this.Item1, @this.Item2);
+}
