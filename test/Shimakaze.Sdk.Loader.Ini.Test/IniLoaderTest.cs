8f8ed8d8119c518d2253339fa43f48813318b857
frg2089 <frg2089@outlook.com>
2022-08-19 16:27:13 +0800

    :white_check_mark: test(.net/ini): 添加测试项目

---
+++ test/Shimakaze.Sdk.Loader.Ini.Test/IniLoaderTest.cs
@@ -0,0 +1,66 @@
+namespace Shimakaze.Sdk.Loader.Ini.Test;
+
+[TestClass]
+public class IniLoaderTest
+{
+    [TestMethod]
+    public void ReadTest()
+    {
+        IniLoader loader = new();
+
+        using StreamReader sr = File.OpenText(Path.Combine("Assets", "Test1.ini"));
+        var ini = loader.Read(sr, default);
+
+        Assert.IsNotNull(ini);
+        Assert.IsTrue(ini.Default[0].IsEmptyKey);
+        Assert.IsTrue(ini.Default[0].IsEmptyValue);
+        Assert.IsTrue(!ini.Default[0].IsEmptySummary);
+        Assert.IsTrue(ini.Default[0].Summary == "File Summary");
+
+        Assert.IsTrue(ini.Sections[0].Name == "Section1");
+        Assert.IsNotNull(ini.Sections[0].BeforeSummaries);
+        Assert.IsTrue(string.Join("\n", ini.Sections[0].BeforeSummaries!) == "; Before Summary");
+        Assert.IsTrue(ini.Sections[0].Count() == 3);
+
+        Assert.IsTrue(!ini.Sections[0][0].IsEmptyKey);
+        Assert.IsTrue(ini.Sections[0][0].Key == "Key1");
+        Assert.IsTrue(!ini.Sections[0][0].IsEmptyValue);
+        Assert.IsTrue(ini.Sections[0][0].Value!.ToString() == "Value1");
+        Assert.IsTrue(ini.Sections[0][0].IsEmptySummary);
+
+        Assert.IsTrue(!ini.Sections[0][1].IsEmptyKey);
+        Assert.IsTrue(ini.Sections[0][1].Key == "Key2");
+        Assert.IsTrue(!ini.Sections[0][1].IsEmptyValue);
+        Assert.IsTrue(ini.Sections[0][1].Value!.ToString() == "Value2");
+        Assert.IsTrue(!ini.Sections[0][1].IsEmptySummary);
+        Assert.IsTrue(ini.Sections[0][1].Summary == "Inline Sumamry");
+
+        Assert.IsTrue(ini.Sections[0][2].IsEmptyKey);
+        Assert.IsTrue(ini.Sections[0][2].IsEmptyValue);
+        Assert.IsTrue(!ini.Sections[0][2].IsEmptySummary);
+        Assert.IsTrue(ini.Sections[0][2].Summary == "After Summary");
+
+        Assert.IsTrue(ini.Sections[1].Name == "Section2");
+        Assert.IsNotNull(ini.Sections[1].BeforeSummaries);
+        Assert.IsTrue(string.Join("\n", ini.Sections[1].BeforeSummaries!) == "; Before Summary");
+        Assert.IsTrue(ini.Sections[1].Count() == 3);
+
+        Assert.IsTrue(!ini.Sections[1][0].IsEmptyKey);
+        Assert.IsTrue(ini.Sections[1][0].Key == "Key1");
+        Assert.IsTrue(!ini.Sections[1][0].IsEmptyValue);
+        Assert.IsTrue(ini.Sections[1][0].Value!.ToString() == "Value1");
+        Assert.IsTrue(ini.Sections[1][0].IsEmptySummary);
+
+        Assert.IsTrue(!ini.Sections[1][1].IsEmptyKey);
+        Assert.IsTrue(ini.Sections[1][1].Key == "Key2");
+        Assert.IsTrue(!ini.Sections[1][1].IsEmptyValue);
+        Assert.IsTrue(ini.Sections[1][1].Value!.ToString() == "Value2");
+        Assert.IsTrue(!ini.Sections[1][1].IsEmptySummary);
+        Assert.IsTrue(ini.Sections[1][1].Summary == "Inline Sumamry");
+
+        Assert.IsTrue(ini.Sections[1][2].IsEmptyKey);
+        Assert.IsTrue(ini.Sections[1][2].IsEmptyValue);
+        Assert.IsTrue(!ini.Sections[1][2].IsEmptySummary);
+        Assert.IsTrue(ini.Sections[1][2].Summary == "After Summary");
+    }
+}
