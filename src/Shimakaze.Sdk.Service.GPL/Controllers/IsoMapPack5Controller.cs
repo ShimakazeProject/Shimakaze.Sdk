f278e3912e503eb2041be88aafead6ad3c7c9ae3
frg2089 <frg2089@outlook.com>
2022-08-19 15:30:46 +0800

    :sparkles: feat(.net/gpl): 依赖GPL库的项目

---
+++ src/Shimakaze.Sdk.Service.GPL/Controllers/IsoMapPack5Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using lzo.net;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shimakaze.Sdk.Service.GPL.Services;
+
+[Route("/v1/[controller]"), ApiController]
+public class IsoMapPack5Controller : Controller
+{
+    [HttpPost("Decompress")]
+    public async Task DecompressAsync()
+    {
+        using BinaryReader br = new(Request.Body);
+        byte[] buffer = new byte[8192];
+        while (br.PeekChar() >= 0)
+        {
+            var blockSize = br.ReadUInt16();
+            var outputSize = br.ReadUInt16();
+            var lzo = br.ReadBytes(blockSize);
+
+            await using MemoryStream ms = new(lzo);
+            using LzoStream stream = new(ms, CompressionMode.Decompress);
+            await stream.ReadAsync(buffer.AsMemory(0, outputSize)).ConfigureAwait(false);
+            await Response.BodyWriter.WriteAsync(buffer).ConfigureAwait(false);
+        }
+        await Response.BodyWriter.FlushAsync().ConfigureAwait(false);
+    }
+}
