9c84a4e9203b45cf55fdc0bbb7c43f4e0a78b3f5
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-12-17 12:07:40 +0800

    :sparkles: yaml转换器
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Text.Yaml.Csf/Serialization/CsfYamlV1Serializer.cs
@@ -0,0 +1,69 @@
+using Shimakaze.Sdk.Data.Csf;
+using Shimakaze.Sdk.Serialization;
+using Shimakaze.Sdk.Text.Yaml.Csf.Converter.V1;
+
+using YamlDotNet.Serialization;
+
+namespace Shimakaze.Sdk.Text.Yaml.Csf.Serialization;
+
+/// <summary>
+/// CSF YAML Serializer.
+/// </summary>
+public sealed class CsfYamlV1Serializer : ITextSerializer<CsfDocument, object>
+{
+    /// <summary>
+    /// Deserialize.
+    /// </summary>
+    /// <param name="reader">reader.</param>
+    /// <param name="options">options.</param>
+    /// <returns><see cref="CsfDocument"/>.</returns>
+    public static CsfDocument Deserialize(TextReader reader, object? options = null)
+    {
+        return new DeserializerBuilder()
+          .WithTypeConverter(CsfValueConverter.Instance)
+          .WithTypeConverter(CsfDataConverter.Instance)
+          .WithTypeConverter(CsfDocumentConverter.Instance)
+          .Build()
+          .Deserialize<CsfDocument>(reader);
+    }
+
+    /// <summary>
+    /// Deserialize.
+    /// </summary>
+    /// <param name="stream">stream.</param>
+    /// <param name="options">options.</param>
+    /// <returns><see cref="CsfDocument"/>.</returns>
+    public static CsfDocument Deserialize(Stream stream, object? options = null)
+    {
+        using StreamReader sr = new(stream, leaveOpen: true);
+        return Deserialize(sr, options);
+    }
+
+    /// <summary>
+    /// Serialize.
+    /// </summary>
+    /// <param name="writer">writer.</param>
+    /// <param name="document"><see cref="CsfDocument"/>.</param>
+    /// <param name="options">options.</param>
+    public static void Serialize(TextWriter writer, CsfDocument document, object? options = null)
+    {
+        new SerializerBuilder()
+          .WithTypeConverter(CsfValueConverter.Instance)
+          .WithTypeConverter(CsfDataConverter.Instance)
+          .WithTypeConverter(CsfDocumentConverter.Instance)
+          .Build()
+          .Serialize(writer, document);
+    }
+
+    /// <summary>
+    /// Serialize.
+    /// </summary>
+    /// <param name="stream">stream.</param>
+    /// <param name="document"><see cref="CsfDocument"/>.</param>
+    /// <param name="options">options.</param>
+    public static void Serialize(Stream stream, CsfDocument document, object? options = null)
+    {
+        using StreamWriter sw = new(stream, leaveOpen: true);
+        Serialize(sw, document, options);
+    }
+}
---
+++ test/Shimakaze.Sdk.Text.Yaml.Csf.Tests/Serialization/CsfYamlV1SerializerTests.cs
@@ -0,0 +1,67 @@
+﻿using Shimakaze.Sdk.Data.Csf;
+using Shimakaze.Sdk.Data.Csf.Serialization;
+
+namespace Shimakaze.Sdk.Text.Yaml.Csf.Serialization.Tests;
+
+[TestClass()]
+public class CsfYamlV1SerializerTests
+{
+    private const string Assets = "Assets";
+    private const string InputFile = "ra2md.csf";
+    private const string InputFile2 = "ra2md.csf.yaml";
+    private const string OutputPath = "Out";
+
+    [TestMethod()]
+    public void DeserializeTest()
+    {
+        using StreamReader sr = File.OpenText(Path.Combine(Assets, InputFile2));
+        CsfDocument doc = CsfYamlV1Serializer.Deserialize(sr);
+        using Stream output = File.Create(Path.Combine(OutputPath, "DeserializeTest.csf"));
+        using Stream output1 = File.Create(Path.Combine(OutputPath, "DeserializeTest.csf.yml"));
+        CsfSerializer.Serialize(output, doc);
+        CsfYamlV1Serializer.Serialize(output1, doc);
+    }
+
+    [TestMethod()]
+    public void DeserializeTest1()
+    {
+        using Stream stream = File.OpenRead(Path.Combine(Assets, InputFile2));
+        CsfDocument doc = CsfYamlV1Serializer.Deserialize(stream);
+        using Stream output = File.Create(Path.Combine(OutputPath, "DeserializeTest1.csf"));
+        using Stream output1 = File.Create(Path.Combine(OutputPath, "DeserializeTest1.csf.yml"));
+        CsfSerializer.Serialize(output, doc);
+        CsfYamlV1Serializer.Serialize(output1, doc);
+    }
+
+    [TestMethod()]
+    public void SerializeTest()
+    {
+        using Stream stream = File.OpenRead(Path.Combine(Assets, InputFile));
+        CsfDocument document = CsfSerializer.Deserialize(stream);
+
+        if (!Directory.Exists(OutputPath))
+        {
+            Directory.CreateDirectory(OutputPath);
+        }
+
+        string path = Path.Combine(OutputPath, "SerializeTest.csf.yaml");
+        using StreamWriter sw = File.CreateText(path);
+        CsfYamlV1Serializer.Serialize(sw, document);
+    }
+
+    [TestMethod()]
+    public void SerializeTest1()
+    {
+        using Stream stream1 = File.OpenRead(Path.Combine(Assets, InputFile));
+        CsfDocument document = CsfSerializer.Deserialize(stream1);
+
+        if (!Directory.Exists(OutputPath))
+        {
+            Directory.CreateDirectory(OutputPath);
+        }
+
+        string path = Path.Combine(OutputPath, "SerializeTest1.csf.yaml");
+        using Stream stream = File.Create(path);
+        CsfYamlV1Serializer.Serialize(stream, document);
+    }
+}
