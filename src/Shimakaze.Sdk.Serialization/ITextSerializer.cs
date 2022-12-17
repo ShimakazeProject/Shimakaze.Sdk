990b52c64e6b576881fb1e3f2fb4a4ac3015cf20
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-12-17 11:18:22 +0800

    :bricks: 基础结构
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Serialization/ITextSerializer.cs
@@ -0,0 +1,25 @@
+namespace Shimakaze.Sdk.Serialization;
+
+/// <summary>
+/// Text serializer.
+/// </summary>
+/// <typeparam name="TDocument">Document Type.</typeparam>
+/// <typeparam name="TOptions">Options Type.</typeparam>
+public interface ITextSerializer<TDocument, TOptions> : ISerializer<TDocument, TOptions>
+{
+    /// <summary>
+    /// Deserialize.
+    /// </summary>
+    /// <param name="reader">Text Reader.</param>
+    /// <param name="options">Options.</param>
+    /// <returns>Document.</returns>
+    static abstract TDocument Deserialize(TextReader reader, TOptions? options = default);
+
+    /// <summary>
+    /// Serialize.
+    /// </summary>
+    /// <param name="writer">Text Writer.</param>
+    /// <param name="document">Document.</param>
+    /// <param name="options">Options.</param>
+    static abstract void Serialize(TextWriter writer, TDocument document, TOptions? options = default);
+}
