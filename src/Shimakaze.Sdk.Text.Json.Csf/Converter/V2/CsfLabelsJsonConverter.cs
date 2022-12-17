6b5bcb09fd54f506be9d69d231b67d038514fa07
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-12-17 10:05:40 +0800

    :sparkles: json转换器
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Text.Json.Csf/Converter/V2/CsfLabelsJsonConverter.cs
@@ -0,0 +1,45 @@
+﻿using Shimakaze.Sdk.Data.Csf;
+
+namespace Shimakaze.Sdk.Text.Json.Csf.Converter.V2;
+
+/// <summary>
+/// CsfLabelsJsonConverter.
+/// </summary>
+public class CsfLabelsJsonConverter : JsonConverter<IList<CsfData>>
+{
+    /// <inheritdoc/>
+    public override IList<CsfData> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        JsonConverter<CsfData> converter = options.GetConverter<CsfData>();
+        List<CsfData> result = new();
+        while (reader.Read())
+        {
+            if (reader.TokenType is JsonTokenType.EndObject)
+            {
+                break;
+            }
+
+            result.Add(converter.Read(ref reader, options)!);
+        }
+
+        return result.ToArray();
+    }
+
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, IList<CsfData> value, JsonSerializerOptions options)
+    {
+        JsonConverter<CsfData> converter = options.GetConverter<CsfData>();
+        writer.WriteStartObject();
+        foreach (CsfData item in value)
+        {
+            converter.Write(writer, item, options);
+        }
+
+        writer.WriteEndObject();
+    }
+}
---
+++ src/Shimakaze.Sdk.Text.Json.Csf/InternalUtils.cs
@@ -0,0 +1,29 @@
+﻿namespace Shimakaze.Sdk.Text.Json.Csf;
+
+/// <summary>
+/// InternalUtils.
+/// </summary>
+internal static class InternalUtils
+{
+    /// <summary>
+    /// GetConverter.
+    /// </summary>
+    /// <typeparam name="T">T.</typeparam>
+    /// <param name="options">options.</param>
+    /// <returns><see cref="JsonConverter{T}"/>.</returns>
+    /// <exception cref="JsonException">Cannot Find <see cref="JsonConverter{T}"/>.</exception>
+    public static JsonConverter<T> GetConverter<T>(this JsonSerializerOptions options)
+        => options.GetConverter(typeof(T)) as JsonConverter<T> ?? throw new JsonException($"Cannot Find JsonConverter<{typeof(T)}>.");
+
+    /// <summary>
+    /// Read.
+    /// </summary>
+    /// <typeparam name="T">T.</typeparam>
+    /// <param name="this"><see cref="JsonConverter{T}"/>.</param>
+    /// <param name="reader">reader.</param>
+    /// <param name="options">options.</param>
+    /// <returns>T object.</returns>
+    /// <exception cref="JsonException">Cannot Reads and converts the JSON to type T.</exception>
+    internal static T Read<T>(this JsonConverter<T> @this, ref Utf8JsonReader reader, JsonSerializerOptions options)
+        => @this.Read(ref reader, typeof(T), options) ?? throw new JsonException($"Cannot Reads and converts the JSON to type {typeof(T)}.");
+}
