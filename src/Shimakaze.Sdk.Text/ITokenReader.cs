a744309d78a774c42756ee484edbcccdccbbc34c
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-12-17 11:18:22 +0800

    :bricks: 基础结构
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Text/ITokenReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace Shimakaze.Sdk.Text;
+
+/// <summary>
+/// Token Reader.
+/// </summary>
+/// <typeparam name="TToken">Token Type.</typeparam>
+public interface ITokenReader<TToken>
+    where TToken : struct, INumberBase<TToken>
+{
+    /// <summary>
+    /// Gets current value.
+    /// </summary>
+    string Value { get; }
+
+    /// <summary>
+    /// Gets current token.
+    /// </summary>
+    TToken? Token { get; }
+
+    /// <summary>
+    /// Read Next.
+    /// </summary>
+    /// <returns>Success.</returns>
+    bool Read();
+}
