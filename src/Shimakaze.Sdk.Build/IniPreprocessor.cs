df55b366d5fe7449afbe02973482624c871a234a
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-03-05 08:00:40 +0800

    :sparkles: MSBuild INI预处理器
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Build/IniPreprocessor.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Extensions.DependencyInjection;
+
+using Shimakaze.Sdk.Compiler.Preprocessor;
+using Shimakaze.Sdk.Compiler.Preprocessor.Kernel;
+using Shimakaze.Sdk.IO.Mix;
+
+using MSTask = Microsoft.Build.Utilities.Task;
+
+namespace Shimakaze.Sdk.Build;
+
+/// <summary>
+/// Ini 预处理器
+/// </summary>
+public sealed class IniPreprocessor : MSTask
+{
+    /// <summary>
+    /// 将要被预处理的文件列表
+    /// </summary>
+    [Required]
+    public required string Files { get; set; }
+
+    /// <summary>
+    /// 符号
+    /// </summary>
+    [Required]
+    public required string Defines { get; set; }
+
+    /// <summary>
+    /// 目标目录
+    /// </summary>
+    [Required]
+    public required string TargetDirectory { get; set; }
+
+    /// <summary>
+    /// 项目目录
+    /// </summary>
+    [Required]
+    public required string BaseDirectory { get; set; }
+
+    /// <inheritdoc/>
+    public override bool Execute()
+    {
+        ServiceCollection services = new();
+        services
+            .AddPreprocessor(i => i.AddDefines(Defines.Split(';').Select(i => i.Trim())))
+            .AddRegionCommands()
+            .AddConditionCommands()
+            .AddDefineCommands();
+
+        using ServiceProvider serviceProvider = services.BuildServiceProvider();
+        IPreprocessor preprocessor = serviceProvider.GetRequiredService<IPreprocessor>();
+
+        List<Task> tasks = new();
+        foreach (var file in Files.Split(';').Select(i => i.Trim()).Select(Path.GetFullPath))
+        {
+            using var source = File.OpenText(file);
+            using var target = File.CreateText(
+                Path.GetFileNameWithoutExtension(file.Replace(BaseDirectory, TargetDirectory))
+                + ".g.pp"
+                + Path.GetExtension(file)
+            );
+
+            tasks.Add(preprocessor.ExecuteAsync(source, target, file));
+        }
+
+        Task.WaitAll(tasks.ToArray());
+
+        return true;
+    }
+}
