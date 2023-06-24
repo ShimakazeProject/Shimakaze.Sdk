b2745c9b3b1fa0565aa955d227628c4cb0db0196
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-06-24 11:03:08 +0800

    :sparkles: 多线程创建解决方案
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- scripts/Build-Solutions.ps1
+++ scripts/Build-Solutions.ps1
@@ -1,13 +1,39 @@
 #Requires -Version 7.3
 
 $ProjectRoot = Join-Path $PSScriptRoot '..'
+$SrcRoot = Join-Path $ProjectRoot 'src'
 $TestRoot = Join-Path $ProjectRoot 'test'
+$SlnRoot = Join-Path $ProjectRoot 'sln'
 
-Get-ChildItem $TestRoot `
+Write-Host -ForegroundColor Green '##########################'
+Write-Host -ForegroundColor Green '#                        #'
+Write-Host -ForegroundColor Green '#  Solution Generator    #'
+Write-Host -ForegroundColor Green '#            -- frg2089  #'
+Write-Host -ForegroundColor Green '#                        #'
+Write-Host -ForegroundColor Green '##########################'
+
+$env:DOTNET_CLI_UI_LANGUAGE = "en_US"
+
+Get-ChildItem -Path (Join-Path $SlnRoot '*.sln') | Remove-Item
+
+Get-ChildItem $SrcRoot `
 | Where-Object {
   $PSItem -is [System.IO.DirectoryInfo]
 } `
 | ForEach-Object {
-  Write-Output "Generate $($PSItem.Name.Replace('.Tests', ''))"
-  dotnet build $PSItem.FullName -t:SlnGen -p:SlnGenLaunchVisualStudio=False
-}
+  $TestProject = Join-Path $TestRoot "$($PSItem.Name).Tests"
+  if (Test-Path $TestProject) {
+    Get-Item $TestProject
+    return
+  }
+
+  $PSItem
+} `
+| ForEach-Object {
+  $Name = $PSItem.Name.Replace('.Tests', '')
+  Write-Host -ForegroundColor Blue "Generating ""$Name.sln"" ..."
+  Start-Job -ScriptBlock {
+    dotnet build $input -t:SlnGen -p:SlnGenLaunchVisualStudio=False
+  } -InputObject $PSItem.FullName
+} | Wait-Job | Remove-Job | Out-Null
+Write-Host -ForegroundColor Green 'All Done !'
