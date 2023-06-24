1728612c2a72b58ad1bf70d50bfca50155e281ca
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-06-24 18:17:47 +0800

    :recycle: 不提供Int128与字符串互转的属性
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Sdk.Vxl/LimbHeader.cs
+++ src/Shimakaze.Sdk.Vxl/LimbHeader.cs
@@ -9,8 +9,11 @@ namespace Shimakaze.Sdk.Vxl;
 [StructLayout(LayoutKind.Explicit, Pack = 1, Size = 16 + 3 * sizeof(int))]
 public record struct LimbHeader
 {
-    [FieldOffset(0)]
-    private Int128 _name;
+    /// <summary>
+    /// Limb 的名字
+    /// </summary>
+    [field: FieldOffset(0)]
+    public Int128 Name { get; set; }
 
     /// <summary>
     /// Number
@@ -29,32 +32,4 @@ public record struct LimbHeader
     /// </summary>
     [field: FieldOffset(16 + sizeof(int) * 2)]
     public int Unknown2 { get; set; }
-
-    /// <summary>
-    /// Limb 的名字
-    /// </summary>
-    public string Name
-    {
-        readonly get
-        {
-            unsafe
-            {
-                var tmp = _name;
-                return new string((sbyte*)&tmp, 0, 16).Split('\0').First();
-            }
-        }
-        set
-        {
-            unsafe
-            {
-                byte* tmp = stackalloc byte[16];
-                char[] chars = value.ToCharArray();
-                fixed (char* pChar = chars)
-                {
-                    int data = Encoding.ASCII.GetBytes(pChar, chars.Length, tmp, 16);
-                    _name = *(Int128*)tmp;
-                }
-            }
-        }
-    }
 }
