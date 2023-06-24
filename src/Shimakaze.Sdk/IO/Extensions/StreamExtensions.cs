4b5689b9d99724062293d481b363b4fb1f845fff
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-06-24 18:17:28 +0800

    :recycle: 使流实用工具不再需要 buffer 提升性能
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Sdk/IO/Extensions/StreamExtensions.cs
+++ src/Shimakaze.Sdk/IO/Extensions/StreamExtensions.cs
@@ -10,49 +10,13 @@ public static unsafe class StreamExtensions
     /// </summary>
     /// <typeparam name="T">非托管结构体</typeparam>
     /// <param name="stream">流</param>
-    /// <param name="buffer">缓冲区</param>
     /// <param name="destination">目标结构体</param>
-    /// <param name="start">缓冲区起始位置</param>
-    /// <param name="length">缓冲区可用长度</param>
     /// <exception cref="OverflowException"></exception>
-    public static void Read<T>(this Stream stream, byte[] buffer, out T destination, int start = 0, int? length = null)
+    public static void Read<T>(this Stream stream, out T destination)
             where T : unmanaged
     {
-        length ??= buffer.Length - start;
-        // 结构体长度
-        int tSize = sizeof(T);
-
-        if (length < tSize)
-            throw new OverflowException("Buffer is too short!");
-
-        stream.Read(buffer.AsSpan(start, tSize));
-        fixed (byte* ptr = buffer)
-            destination = *(T*)ptr;
-    }
-
-    /// <summary>
-    /// 读取结构体
-    /// </summary>
-    /// <typeparam name="T">非托管结构体</typeparam>
-    /// <param name="stream">流</param>
-    /// <param name="buffer">缓冲区</param>
-    /// <param name="start">缓冲区起始位置</param>
-    /// <param name="length">缓冲区可用长度</param>
-    /// <returns>值</returns>
-    /// <exception cref="OverflowException"></exception>
-    public static T Read<T>(this Stream stream, byte[] buffer, int start = 0, int? length = null)
-            where T : unmanaged
-    {
-        length ??= buffer.Length - start;
-        // 结构体长度
-        int tSize = sizeof(T);
-
-        if (length < tSize)
-            throw new OverflowException("Buffer is too short!");
-
-        stream.Read(buffer.AsSpan(start, tSize));
-        fixed (byte* ptr = buffer)
-            return *(T*)ptr;
+        fixed (T* ptr = &destination)
+            stream.Read(new Span<byte>(ptr, sizeof(T)));
     }
 
     /// <summary>
@@ -60,49 +24,13 @@ public static unsafe class StreamExtensions
     /// </summary>
     /// <typeparam name="T">非托管结构体</typeparam>
     /// <param name="stream">流</param>
-    /// <param name="buffer">缓冲区</param>
     /// <param name="destination">目标数组</param>
-    /// <param name="start">缓冲区起始位置</param>
-    /// <param name="length">缓冲区可用长度</param>
     /// <exception cref="OverflowException"></exception>
-    public static void Read<T>(this Stream stream, byte[] buffer, in T[] destination, int start = 0, int? length = null)
+    public static void Read<T>(this Stream stream, in T[] destination)
         where T : unmanaged
     {
-        length ??= buffer.Length - start;
-        // 结构体长度
-        int tSize = sizeof(T);
-
-        if (length < tSize)
-            throw new OverflowException("Buffer is too short!");
-
-        // 使缓冲区可用长度始终是 结构体长度 的倍数
-        if (length % tSize is not 0)
-            length = length / tSize * tSize;
-
-        for (int i = 0; i < destination.Length;)
-        {
-            // 剩余个数
-            int tRemain = destination.Length - i;
-            // 剩余长度
-            int tRemainSize = tRemain * tSize;
-            // 读取长度 如果 剩余长度 > 缓冲区长度 则使用 缓冲区长度 否则使用 剩余长度
-            int size = tRemainSize > length ? length.Value : tRemainSize;
-
-            // 读取数据
-            stream.Read(buffer.AsSpan(start, size));
-
-            // 复制内存
-            fixed (byte* pSrc = buffer)
-            fixed (T* pDest = destination)
-                Buffer.MemoryCopy(
-                    pSrc + start,
-                    pDest + i,
-                    size,
-                    size
-                );
-
-            i += size / tSize;
-        }
+        fixed (T* ptr = destination)
+            stream.Read(new Span<byte>(ptr, destination.Length * sizeof(T)));
     }
 
     /// <summary>
@@ -115,10 +43,8 @@ public static unsafe class StreamExtensions
     public static void Write<T>(this Stream stream, in T value)
         where T : unmanaged
     {
-        // 结构体长度
-        int tSize = sizeof(T);
         fixed (T* ptr = &value)
-            stream.Write(new Span<byte>(ptr, tSize));
+            stream.Write(new Span<byte>(ptr, sizeof(T)));
     }
 
     /// <summary>
@@ -131,9 +57,7 @@ public static unsafe class StreamExtensions
     public static void Write<T>(this Stream stream, in T[] value)
         where T : unmanaged
     {
-        // 结构体长度
-        int tSize = sizeof(T);
         fixed (T* ptr = value)
-            stream.Write(new Span<byte>(ptr, tSize * value.Length));
+            stream.Write(new Span<byte>(ptr, value.Length * sizeof(T)));
     }
 }
