dfef819e67f724ba5583dc31d4a1e1651e18fd77
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-06-24 18:17:44 +0800

    :sparkles: PAL色板文件
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Pal/IO/PaletteReader.cs
@@ -0,0 +1,30 @@
+﻿using Shimakaze.Sdk.Pal;
+
+namespace Shimakaze.Sdk.IO.Pal;
+/// <summary>
+/// PaletteReader
+/// </summary>
+public sealed class PaletteReader : IReader<Palette>
+{
+    private readonly byte[] _bytes;
+
+    /// <summary>
+    /// PaletteReader
+    /// </summary>
+    public PaletteReader(byte[] bytes)
+    {
+        _bytes = bytes;
+    }
+
+    /// <inheritdoc/>
+    public Palette Read()
+    {
+        unsafe
+        {
+            fixed (byte* ptr = _bytes)
+            {
+                return *(Palette*)ptr;
+            }
+        }
+    }
+}
---
+++ src/Shimakaze.Sdk.Pal/IO/PaletteWriter.cs
@@ -0,0 +1,29 @@
+﻿using Shimakaze.Sdk.Pal;
+
+namespace Shimakaze.Sdk.IO.Pal;
+
+/// <summary>
+/// PaletteWriter
+/// </summary>
+public sealed class PaletteWriter : IWriter<Palette>
+{
+    private readonly BinaryWriter _writer;
+
+    /// <summary>
+    /// PaletteWriter
+    /// </summary>
+    public PaletteWriter(BinaryWriter writer)
+    {
+        _writer = writer;
+    }
+    /// <inheritdoc/>
+
+    public void Write(in Palette value)
+    {
+        unsafe
+        {
+            Palette pal = value;
+            _writer.Write(new Span<byte>((byte*)&pal, sizeof(Palette)));
+        }
+    }
+}
