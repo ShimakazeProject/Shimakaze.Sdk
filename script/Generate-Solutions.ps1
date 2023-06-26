cc6b38511101ba13b0ffef858878d37eff35e592
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-06-26 11:06:08 +0800

    :construction_worker: 更新CI
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ script/Generate-Solutions.ps1
@@ -0,0 +1,26 @@
+#Requires -Version 7.3
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]
+    $NoParallel
+)
+
+. $PSScriptRoot/Invoke-AllProject.ps1
+
+$ProjectRoot = Join-Path $PSScriptRoot '..'
+$SlnRoot = Join-Path $ProjectRoot 'sln'
+
+Write-Host -ForegroundColor Green '##########################'
+Write-Host -ForegroundColor Green '#                        #'
+Write-Host -ForegroundColor Green '#  Solution Generator    #'
+Write-Host -ForegroundColor Green '#            -- frg2089  #'
+Write-Host -ForegroundColor Green '#                        #'
+Write-Host -ForegroundColor Green '##########################'
+
+Get-ChildItem -Path (Join-Path $SlnRoot '*.sln') | Remove-Item
+
+Invoke-AllProject {
+  dotnet build $args -t:SlnGen -p:SlnGenLaunchVisualStudio=False
+} -Parallel:(!$NoParallel) -Echo "Generating ""{0}.sln"""
---
+++ script/Invoke-AllSolutions.ps1
@@ -0,0 +1,42 @@
+#Requires -Version 7.3
+
+function Invoke-AllProject {
+  [CmdletBinding()]
+  param (
+    [scriptblock]
+    $Action,
+    [switch]
+    $Parallel,
+    [string]
+    $Echo
+  )
+
+  begin {
+    $ProjectRoot = Join-Path $PSScriptRoot '..'
+    $SlnRoot = Join-Path $ProjectRoot 'sln'
+
+    $env:DOTNET_CLI_UI_LANGUAGE = "en_US"
+  }
+
+  process {
+    $result = Get-ChildItem -Path $SlnRoot/*.sln `
+    | ForEach-Object {
+      $Name = $PSItem.Name.Replace('.Tests', '')
+      $Display = $Echo -f $Name
+      Write-Host -ForegroundColor Blue $Display
+      if ($Parallel) {
+        Start-Job -ScriptBlock $Action -ArgumentList $PSItem.FullName -WorkingDirectory $ProjectRoot
+      }
+      else {
+        Invoke-Command -ScriptBlock $Action -ArgumentList $PSItem.FullName | Receive-PSSession
+      }
+    }
+  }
+
+  end {
+    if ($Parallel) {
+      $result | Wait-Job | Remove-Job | Out-Null
+    }
+    Write-Host -ForegroundColor Green 'All Done !'
+  }
+}
