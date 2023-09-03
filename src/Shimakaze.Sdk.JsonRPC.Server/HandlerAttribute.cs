58d75dcf71c7ee99ac9dbaf43bd6772e95de2c0c
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-09-03 20:02:31 +0800

    :recycle: 重构JsonRPC服务器代码
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Sdk.JsonRPC.Server/HandlerAttribute.cs
+++ src/Shimakaze.Sdk.JsonRPC.Server/HandlerAttribute.cs
@@ -1,22 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
 namespace Shimakaze.Sdk.JsonRPC.Server;
 
 /// <summary>
 /// 表示这是一个JsonRpc Handler
 /// </summary>
-[AttributeUsage(AttributeTargets.Class)]
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
 public sealed class HandlerAttribute : Attribute
 {
     /// <summary>
     /// 这个Handler中的方法/事件的路径
     /// </summary>
-    public string? Route { get; set; }
+    [StringSyntax("Uri")]
+    public string? Route { get; init; }
 
     /// <summary>
     /// 表示这是一个JsonRpc Handler
     /// </summary>
     /// <param name="route"> 这个Handler中的方法/事件的路径 </param>
-    public HandlerAttribute(string? route = null)
-    {
-        Route = route;
-    }
+    public HandlerAttribute([StringSyntax("Uri")]string? route = default) => Route = route;
 }
