296b0958e2f6c4c42aea897070e8ff89f8d83939
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-11-27 19:00:20 +0800

    实现 Ares INI对象
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk/Ini/Ares/AresIniDocument.cs
@@ -0,0 +1,18 @@
+﻿namespace Shimakaze.Sdk.Ini.Ares;
+
+/// <inheritdoc/>
+public sealed class AresIniDocument : IniDocument<AresIniSection>
+{
+    /// <inheritdoc/>
+    public AresIniDocument()
+    {
+    }
+
+    /// <inheritdoc/>
+    public AresIniDocument(IEnumerable<AresIniSection> sections) : base(sections)
+    {
+    }
+
+    /// <inheritdoc/>
+    public override AresIniSection Default { get; } = new() { Name = ";Default;" };
+}
