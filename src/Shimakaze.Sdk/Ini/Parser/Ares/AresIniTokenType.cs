139331e41fdb2ce1f5fff9dd68b1c189c0f3dc72
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-11-28 11:24:26 +0800

    实现 Ares INI 语法树读取器
    修复基节优先级过高的BUG
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk/Ini/Parser/Ares/AresIniTokenType.cs
@@ -0,0 +1,22 @@
+﻿namespace Shimakaze.Sdk.Ini.Parser.Ares;
+
+/// <summary>
+/// Ares INI Token Type
+/// </summary>
+public static class AresIniTokenType
+{
+    /// <summary>
+    /// Ares INI 特有的 继承 Section
+    /// </summary>
+    /// <remarks>
+    /// [Section] : [Base]
+    /// </remarks>
+    public const int BASE_SECTION = ':';
+    /// <summary>
+    /// PLUS
+    /// </summary>
+    /// <remarks>
+    /// += Value
+    /// </remarks>
+    public const int PLUS = '+';
+}
