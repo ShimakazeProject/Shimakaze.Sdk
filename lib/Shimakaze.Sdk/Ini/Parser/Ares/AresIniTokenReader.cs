df70b7eebc1ca11d191d676a087c593cc71be560
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-12-10 12:41:59 +0800

    提升版本号
    修复 INITokenReader 不能读无值键的问题
    更新CI
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- lib/Shimakaze.Sdk/Ini/Parser/Ares/AresIniTokenReader.cs
+++ lib/Shimakaze.Sdk/Ini/Parser/Ares/AresIniTokenReader.cs
@@ -11,24 +11,26 @@ namespace Shimakaze.Sdk.Ini.Parser.Ares;
 public class AresIniTokenReader(TextReader textReader, IniTokenIgnoreLevel ignore = IniTokenIgnoreLevel.NonValue, bool leaveOpen = false) : IniTokenReader(textReader, ignore, leaveOpen)
 {
     /// <inheritdoc/>
-    protected override bool FlushBuffer([NotNullWhen(true)] out IniToken? result, int type = 0)
+    protected override bool FlushBuffer([NotNullWhen(true)] out IniToken? result, int type = IniTokenType.Unknown)
     {
         result = default;
-        if (type is IniTokenType.Unknown
-            && _buffer.Length is not 0
-            && _buffer[0] is ':')
+        if (Buffer.Length is not 0)
         {
-            // 继承节
-            result = new(AresIniTokenType.BASE_SECTION);
-            _buffer.Clear();
-        }
-        else if (type is IniTokenType.Key
-            && _buffer.Length is not 0
-            && _buffer[0] is '+')
-        {
-            // += 注册表
-            result = new(AresIniTokenType.PLUS);
-            _buffer.Clear();
+            switch ((type, Buffer[0]))
+            {
+                case (IniTokenType.Unknown, ':'):
+                    // 继承节
+                    result = new(AresIniTokenType.BASE_SECTION);
+                    Buffer.Clear();
+                    break;
+                case (IniTokenType.Key, '+'):
+                    // += 注册表
+                    result = new(AresIniTokenType.PLUS);
+                    Buffer.Clear();
+                    break;
+                default:
+                    break;
+            }
         }
 
         return result is not null || base.FlushBuffer(out result, type);
---
+++ lib/Shimakaze.Sdk/Ini/Parser/BucketStack.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Shimakaze.Sdk.Ini.Parser;
+
+/// <summary>
+/// 成对符号栈
+/// </summary>
+public sealed class BucketStack
+{
+    private readonly Stack<(char Start, StringBuilder Buffer)> _depths = new();
+    private readonly Stack<StringBuilder> _bufferCaches = new();
+
+    /// <summary>
+    /// 暂存区
+    /// </summary>
+    public StringBuilder Buffer { get; private set; } = new();
+
+    /// <summary>
+    /// 当前符号
+    /// </summary>
+    public char Current => _depths.TryPeek(out var result) ? result.Start : char.MinValue;
+
+    /// <summary>
+    /// 是否是空的
+    /// </summary>
+    public bool Empty => _depths.Count is 0;
+
+    /// <summary>
+    /// 压栈
+    /// </summary>
+    /// <param name="start"></param>
+    public void Push(char start)
+    {
+        _depths.Push((start, Buffer));
+        if (!_bufferCaches.TryPop(out var buffer))
+            buffer = new();
+        Buffer = buffer.Clear();
+    }
+
+    /// <summary>
+    /// 弹栈
+    /// </summary>
+    /// <param name="result"></param>
+    /// <returns></returns>
+    public bool Pop([NotNullWhen(true)] out string? result)
+    {
+        result = default;
+        (_, StringBuilder lastBuffer) = _depths.Pop();
+        if (Buffer is not { Length: 0 })
+            result = Buffer.ToString();
+        _bufferCaches.Push(Buffer);
+        Buffer = lastBuffer;
+        return result is not null;
+    }
+}
