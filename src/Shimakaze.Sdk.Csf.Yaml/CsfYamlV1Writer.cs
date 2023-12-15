de9c3be749a10016314370dad0f95a3638f9055e
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-12-15 16:14:49 +0800

    重构 CSF.Xml CSF.Yaml
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/Shimakaze.Sdk.Csf.Yaml/CsfYamlV1Writer.cs
+++ src/Shimakaze.Sdk.Csf.Yaml/CsfYamlV1Writer.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+
 using Shimakaze.Sdk.Csf.Yaml.Converter.V1;
 
 using YamlDotNet.Serialization;
@@ -7,24 +9,31 @@ namespace Shimakaze.Sdk.Csf.Yaml;
 /// <summary>
 /// CSF YAML Serializer.
 /// </summary>
-/// <remarks>
-/// 构造器
-/// </remarks>
-/// <param name="stream"> 基础流 </param>
+/// <param name="writer"> 基础流 </param>
+/// <param name="builder"></param>
 /// <param name="leaveOpen"> 退出时是否保持流打开 </param>
-public sealed class CsfYamlV1Writer(TextWriter stream, bool leaveOpen = false) : AsyncTextWriter<CsfDocument>(stream, leaveOpen), ICsfWriter
+public sealed class CsfYamlV1Writer(TextWriter writer, Func<SerializerBuilder, SerializerBuilder>? builder = null, bool leaveOpen = false) : ICsfWriter, IDisposable, IAsyncDisposable
 {
+    private readonly DisposableObject<TextWriter> _disposable = new(writer, leaveOpen);
+
+    /// <inheritdoc/>
+    public void Dispose() => _disposable.Dispose();
+
+    /// <inheritdoc/>
+    public ValueTask DisposeAsync() => _disposable.DisposeAsync();
 
     /// <inheritdoc />
-    public override async Task WriteAsync(CsfDocument value, IProgress<float>? progress = default, CancellationToken cancellationToken = default)
+    public async Task WriteAsync(CsfDocument value, IProgress<float>? progress = default, CancellationToken cancellationToken = default)
     {
         await Task.Yield();
 
-        new SerializerBuilder()
-            .WithTypeConverter(CsfValueConverter.Instance)
-            .WithTypeConverter(CsfDataConverter.Instance)
-            .WithTypeConverter(CsfDocumentConverter.Instance)
-            .Build()
-            .Serialize(BaseWriter, value);
+        builder ??= i => i;
+
+        builder(new())
+           .WithTypeConverter(CsfValueConverter.Instance)
+           .WithTypeConverter(CsfDataConverter.Instance)
+           .WithTypeConverter(CsfDocumentConverter.Instance)
+           .Build()
+           .Serialize(_disposable, value);
     }
 }
