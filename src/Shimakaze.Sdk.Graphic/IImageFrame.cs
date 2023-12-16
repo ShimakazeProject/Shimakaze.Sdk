45ad0c60020be5e3f459013451b09422515408a6
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-12-16 16:55:25 +0800

    不完整的Shp解码器
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Graphic/IImageFrame.cs
@@ -0,0 +1,18 @@
+﻿using Shimakaze.Sdk.Graphic.Pixel;
+
+namespace Shimakaze.Sdk.Graphic;
+
+/// <summary>
+/// 图片帧
+/// </summary>
+public interface IImageFrame
+{
+    /// <summary>
+    /// 按特定像素格式写到流
+    /// </summary>
+    /// <typeparam name="TPixel">像素格式</typeparam>
+    /// <param name="stream">流</param>
+    /// <exception cref="NotSupportedException">不受支持的像素格式</exception>
+    void WriteTo<TPixel>(Stream stream)
+        where TPixel : unmanaged, IPixel;
+}
