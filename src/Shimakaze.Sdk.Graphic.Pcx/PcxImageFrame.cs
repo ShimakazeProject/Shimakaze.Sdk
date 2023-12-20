ee71fdcb45988030d15a3dd290c005a0e9b793fa
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-12-20 21:34:09 +0800

    重构PCX解码器
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.Graphic.Pcx/PcxImageFrame.cs
@@ -0,0 +1,37 @@
+﻿using Shimakaze.Sdk.Graphic.Pixel;
+
+namespace Shimakaze.Sdk.Graphic.Pcx;
+
+internal sealed class PcxImageFrame(int width, int height) : IImageFrame
+{
+    public int Width { get; } = width;
+
+    public int Height { get; } = height;
+
+    internal Rgb24[] Pixels { get; } = new Rgb24[width * height];
+
+    public void WriteTo<TPixel>(Stream stream) where TPixel : unmanaged, IPixel
+    {
+        if (typeof(TPixel) == typeof(Rgb24))
+        {
+            foreach (Rgb24 pixel in Pixels)
+                stream.Write(pixel);
+        }
+        else if (typeof(TPixel) == typeof(Rgb565))
+        {
+            foreach (Rgb24 pixel in Pixels)
+            {
+                pixel.ToRgb565(out var target);
+                stream.Write(target);
+            }
+        }
+        else if (typeof(TPixel) == typeof(Rgba32))
+        {
+            foreach (Rgb24 pixel in Pixels)
+            {
+                pixel.ToRgba32(out var target);
+                stream.Write(target);
+            }
+        }
+    }
+}
