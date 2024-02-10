7878005083f7450b0348a54051407e1e91cd2b0e
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-02-10 18:02:49 +0800

    创建语言服务器项目
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ src/Shimakaze.Sdk.LanguageServer/LanguageServerHostedService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+
+namespace Shimakaze.Sdk.LanguageServer;
+
+internal sealed partial class LanguageServerHostedService(
+    ILanguageServer server
+    ) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await server.Initialize(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
