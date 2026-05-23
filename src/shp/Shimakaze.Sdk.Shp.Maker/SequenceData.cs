6cc1823434a45de343852586099ea7fe42c0e8ab
舰队的偶像-岛风酱! <frg2089@outlook.com>
2026-05-24 04:48:10 +0800

    chore: 移除 SixLabors.ImageSharp 依赖及 Shp.Maker 项目
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/shp/Shimakaze.Sdk.Shp.Maker/SequenceData.cs
+++
@@ -1,8 +0,0 @@
-namespace Shimakaze.Sdk.Shp.Maker;
-
-internal sealed record class SequenceData(int Start, int Count, string Angle)
-{
-    public bool HasAngle => int.TryParse(Angle, out _);
-    public int AngleCount => int.TryParse(Angle, out var result) ? result : 1;
-    public int End { get; set; } = Count;
-}
