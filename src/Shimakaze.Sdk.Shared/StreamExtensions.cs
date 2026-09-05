b7e8aceed735e76e608c432e9f951a136587283e
舰队的偶像-岛风酱! <frg2089@outlook.com>
2026-09-05 17:00:52 +0800

    build: 统一目标框架为 net10.0 并移除 polyfill

--- src/Shimakaze.Sdk.Shared/StreamExtensions.cs
+++ src/Shimakaze.Sdk.Shared/StreamExtensions.cs
@@ -16,16 +16,8 @@ internal static class StreamExtensions
     public static void Read<T>(this Stream stream, out T destination)
         where T : unmanaged
     {
-#if !NETCOREAPP2_1_OR_GREATER && !NETSTANDARD2_1
-        unsafe
-        {
-            fixed (T* p = &destination)
-                stream.Read(new Span<T>(p, 1));
-        }
-#else
         destination = default;
         stream.Read(MemoryMarshal.CreateSpan(ref destination, 1));
-#endif
     }
 
     /// <inheritdoc cref="Read{T}(Stream, out T)"/>
@@ -46,34 +38,15 @@ internal static class StreamExtensions
     /// <param name="source"> 结构体 </param>
     public static void Write<T>(this Stream stream, in T source)
         where T : unmanaged
-        => stream.Write(MemoryMarshal.AsBytes([source]));
+        => stream.Write(MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(in source, 1)));
 
     /// <inheritdoc cref="Write{T}(Stream, in T)"/>
     public static void Write<T>(this Stream stream, ReadOnlySpan<T> source)
         where T : unmanaged
-    {
-#if NETSTANDARD2_0
-        // netstandard2.0 的 Stream 没有 Write(ReadOnlySpan<byte>) 实例方法，
-        // 直接调用会重新绑定到本扩展方法自身，造成无限递归。此处退回经典 byte[] API。
-        byte[] buffer = MemoryMarshal.AsBytes(source).ToArray();
-        stream.Write(buffer, 0, buffer.Length);
-#else
-        stream.Write(MemoryMarshal.AsBytes(source));
-#endif
-    }
-
-    /// <inheritdoc cref="Write{T}(Stream, ReadOnlySpan{T})"/>
-    public static void Write<T>(this Stream stream, Span<T> source)
-        where T : unmanaged
-        => stream.Write((ReadOnlySpan<T>)source);
+        => stream.Write(MemoryMarshal.AsBytes(source));
 
     /// <inheritdoc cref="Write{T}(Stream, ReadOnlySpan{T})"/>
     public static void Write<T>(this Stream stream, ReadOnlyMemory<T> source)
-        where T : unmanaged
-        => stream.Write(source.Span);
-
-    /// <inheritdoc cref="Write{T}(Stream, ReadOnlySpan{T})"/>
-    public static void Write<T>(this Stream stream, Memory<T> source)
         where T : unmanaged
         => stream.Write(source.Span);
 }
